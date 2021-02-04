86b1b7e2dd4684788e37520f717deec7d6e3b0f7
igortsvetkov <genrytop@gmail.com>
2021-02-05 00:19:49 +0300

    Start lesson

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <!doctype html>
-<html lang="ru>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PhP Lessons</title>
+    <title>Привет мир</title>
 </head>
 <body>
+<b>
+    <?php
+    print "Привет мир!";
+    ?>
+</b>
 
-<?php
-echo '<h1>Привет Мир</h1>';
-echo  ' привет мир! ';
-echo 1+3;
-echo 1*3;
-echo 6/3;
+<p>
+    <?php
+    print <<<_HTML_
+<form method="post" action="$_SERVER[PHP_SELF]">
+Your Name: <input type="text" name="user" />
+<br/>
+<button type="submit">Say Hello</button>
+</form>
+_HTML_;
 
-?>
+    ?>
+</p>
 
 </body>
 </html>
